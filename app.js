2f075aa78ea58d82a4fdb728ef52ef51ac650293
Hjalmar-K-sson <45142969+Hjalmar-K-sson@users.noreply.github.com>
2021-11-03 19:23:10 +0100

    created new project structure with express-generator & customized it

--- app.js
+++ app.js
@@ -1,12 +1,47 @@
+if (process.env.NODE_ENV !== "production") {
+  require("dotenv").config();
+}
+
+const ejs = require("ejs");
+const createError = require("http-errors");
 const express = require("express");
+const path = require("path");
+const cookieParser = require("cookie-parser");
+const logger = require("morgan");
+
+const restaurantsRouter = require("./routes/restaurants");
+const reviewsRouter = require("./routes/reviews");
+const usersRouter = require("./routes/users");
+
 const app = express();
 
-app.use(express.static("public"));
+// view engine setup
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
 
-app.get("/", (req, res) => {
-  res.render("index.html");
+app.use(logger("dev"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, "public")));
+
+app.use("/", indexRouter);
+app.use("/users", usersRouter);
+
+// catch 404 and forward to error handler
+app.use(function (req, res, next) {
+  next(createError(404));
 });
 
-app.listen(3000, () => {
-  console.log("listening at port 3000. . .");
+// error handler
+app.use(function (err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get("env") === "development" ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render("error");
 });
+
+module.exports = app;
