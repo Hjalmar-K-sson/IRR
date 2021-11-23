9037da3ec9965c9a134a4569aa9d2320a2123721
Hjalmar-K-sson <45142969+Hjalmar-K-sson@users.noreply.github.com>
2021-11-23 20:15:47 +0100

    Added authorization middleware

--- public/javascripts/middleware.js
+++ public/javascripts/middleware.js
@@ -1,8 +1,20 @@
+const Restaurant = require("./models/restaurant");
+
 module.exports.isLoggedIn = (req, res, next) => {
   if (!req.isAuthenticated()) {
     req.session.returnTo = req.originalUrl;
     req.flash("error", "Please sign in!");
     return res.redirect("/login");
+  }
+  next();
+};
+
+module.exports.isAutor = async (req, res, next) => {
+  const { id } = req.params;
+  const restaurant = await Restaurant.findById(id);
+  if (!restaurant.author.equals(req.user.id)) {
+    req.flash("error", "You are not authorized to do this!");
+    return res.redirect(`/restaurants/${id}`);
   }
   next();
 };
