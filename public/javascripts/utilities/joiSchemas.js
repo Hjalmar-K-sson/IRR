73199d85989f6fedb50e57a4dcf38753f7cb2ae4
Hjalmar-K-sson <45142969+Hjalmar-K-sson@users.noreply.github.com>
2021-11-24 20:16:11 +0100

    Fixed type error in joiSchemas file

--- public/javascripts/utilities/joiSchemas.js
+++ public/javascripts/utilities/joiSchemas.js
@@ -2,28 +2,28 @@ const Joi = require("joi");
 
 module.exports.restaurantSchema = Joi.object({
   restaurant: Joi.object({
-    name: Joi.string.required(),
+    name: Joi.string().required(),
     address: {
-      city: Joi.string.required(),
-      state_region: Joi.string,
-      country: Joi.string.required(),
-      street: Joi.string.required(),
-      houseNumber: Joi.string,
-      zipCode: Joi.string,
+      city: Joi.string().required(),
+      state_region: Joi.string(),
+      country: Joi.string().required(),
+      street: Joi.string().required(),
+      houseNumber: Joi.string(),
+      zipCode: Joi.string(),
     },
-    description: Joi.string.required(),
-    priceRange: Joi.string.required(),
-    cuisine: Joi.string.required(),
+    description: Joi.string().required(),
+    priceRange: Joi.string().required(),
+    cuisine: Joi.string().required(),
     courses: {
-      courseName: Joi.string,
+      courseName: Joi.string(),
       courseImgs: {
-        filename: Joi.string,
-        url: Joi.string,
+        filename: Joi.string(),
+        url: Joi.string(),
       },
     },
     images: {
-      filename: Joi.string,
-      url: Joi.string,
+      filename: Joi.string(),
+      url: Joi.string(),
     },
   }).required(),
 });
