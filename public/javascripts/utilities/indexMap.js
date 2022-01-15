52a180f26f038e5beb85240e03203b81d4405bfa
Hjalmar-K-sson <45142969+Hjalmar-K-sson@users.noreply.github.com>
2022-01-15 15:07:52 +0100

    Issue with display of points & clusters on index map WIP

--- public/javascripts/utilities/indexMap.js
+++ public/javascripts/utilities/indexMap.js
@@ -7,58 +7,63 @@ const map = new mapboxgl.Map({
 });
 
 map.on("load", () => {
-  map.addSource("restaurants", {
+  // Add a new source from our GeoJSON data and
+  // set the 'cluster' option to true. GL-JS will
+  // add the point_count property to your source data.
+  map.addSource("earthquakes", {
     type: "geojson",
+    // Point to GeoJSON data. This example visualizes all M1.0+ earthquakes
+    // from 12/22/15 to 1/21/16 as logged by USGS' Earthquake hazards program.
     data: restaurants,
     cluster: true,
-    clusterMaxZoom: 14,
-    clusterRadius: 50,
+    clusterMaxZoom: 14, // Max zoom to cluster points on
+    clusterRadius: 50, // Radius of each cluster when clustering points (defaults to 50)
   });
-
-  map.addControl(new mapboxgl.NavigationControl(), "bottom-right");
 
   map.addLayer({
     id: "clusters",
     type: "circle",
-    source: "restaurants",
+    source: "earthquakes",
     filter: ["has", "point_count"],
     paint: {
+      // Use step expressions (https://docs.mapbox.com/mapbox-gl-js/style-spec/#expressions-step)
+      // with three steps to implement three types of circles:
+      //   * Blue, 20px circles when point count is less than 100
+      //   * Yellow, 30px circles when point count is between 100 and 750
+      //   * Pink, 40px circles when point count is greater than or equal to 750
       "circle-color": [
         "step",
         ["get", "point_count"],
-        "#0D6EFD",
-        10,
-        "#0D6EFD",
-        30,
-        "#0D6EFD",
+        "#51bbd6",
+        100,
+        "#f1f075",
+        750,
+        "#f28cb1",
       ],
-      "circle-radius": ["step", ["get", "point_count"], 25, 10, 20, 30, 25],
+      "circle-radius": ["step", ["get", "point_count"], 20, 100, 30, 750, 40],
     },
   });
 
   map.addLayer({
     id: "cluster-count",
     type: "symbol",
-    source: "restaurants",
+    source: "earthquakes",
     filter: ["has", "point_count"],
     layout: {
       "text-field": "{point_count_abbreviated}",
       "text-font": ["DIN Offc Pro Medium", "Arial Unicode MS Bold"],
       "text-size": 12,
     },
-    paint: {
-      "text-color": "#fff",
-    },
   });
 
   map.addLayer({
     id: "unclustered-point",
     type: "circle",
-    source: "restaurants",
+    source: "earthquakes",
     filter: ["!", ["has", "point_count"]],
     paint: {
-      "circle-color": "#0D6EFD",
-      "circle-radius": 5,
+      "circle-color": "#11b4da",
+      "circle-radius": 4,
       "circle-stroke-width": 1,
       "circle-stroke-color": "#fff",
     },
@@ -71,25 +76,37 @@ map.on("load", () => {
     });
     const clusterId = features[0].properties.cluster_id;
     map
-      .getSource("restaurants")
+      .getSource("earthquakes")
       .getClusterExpansionZoom(clusterId, (err, zoom) => {
         if (err) return;
 
         map.easeTo({
-          center: features[0].address.geometry.coordinates,
+          center: features[0].geometry.coordinates,
           zoom: zoom,
         });
       });
   });
 
+  // When a click event occurs on a feature in
+  // the unclustered-point layer, open a popup at
+  // the location of the feature, with
+  // description HTML from its properties.
   map.on("click", "unclustered-point", (e) => {
-    const coordinates = e.features[0].address.geometry.coordinates.slice();
-    const { popUpMarkup } = e.features[0].properties;
+    const coordinates = e.features[0].geometry.coordinates.slice();
+    const mag = e.features[0].properties.mag;
+    const tsunami = e.features[0].properties.tsunami === 1 ? "yes" : "no";
+
+    // Ensure that if the map is zoomed out such that
+    // multiple copies of the feature are visible, the
+    // popup appears over the copy being pointed to.
     while (Math.abs(e.lngLat.lng - coordinates[0]) > 180) {
       coordinates[0] += e.lngLat.lng > coordinates[0] ? 360 : -360;
     }
 
-    new mapboxgl.Popup().setLngLat(coordinates).setHTML(popUpMarkup).addTo(map);
+    new mapboxgl.Popup()
+      .setLngLat(coordinates)
+      .setHTML(`magnitude: ${mag}<br>Was there a tsunami?: ${tsunami}`)
+      .addTo(map);
   });
 
   map.on("mouseenter", "clusters", () => {
@@ -99,3 +116,97 @@ map.on("load", () => {
     map.getCanvas().style.cursor = "";
   });
 });
+
+// map.on("load", () => {
+//   map.addSource("restaurants", {
+//     type: "geojson",
+//     data: restaurants,
+//     cluster: true,
+//     clusterMaxZoom: 14,
+//     clusterRadius: 50,
+//   });
+
+//   map.addControl(new mapboxgl.NavigationControl(), "bottom-right");
+
+//   map.addLayer({
+//     id: "clusters",
+//     type: "circle",
+//     source: "restaurants",
+//     filter: ["has", "point_count"],
+//     paint: {
+//       "circle-color": [
+//         "step",
+//         ["get", "point_count"],
+//         "#0D6EFD",
+//         10,
+//         "#0D6EFD",
+//         30,
+//         "#0D6EFD",
+//       ],
+//       "circle-radius": ["step", ["get", "point_count"], 25, 10, 20, 30, 25],
+//     },
+//   });
+
+//   map.addLayer({
+//     id: "cluster-count",
+//     type: "symbol",
+//     source: "restaurants",
+//     filter: ["has", "point_count"],
+//     layout: {
+//       "text-field": "{point_count_abbreviated}",
+//       "text-font": ["DIN Offc Pro Medium", "Arial Unicode MS Bold"],
+//       "text-size": 12,
+//     },
+//     paint: {
+//       "text-color": "#fff",
+//     },
+//   });
+
+//   map.addLayer({
+//     id: "unclustered-point",
+//     type: "circle",
+//     source: "restaurants",
+//     filter: ["!", ["has", "point_count"]],
+//     paint: {
+//       "circle-color": "#0D6EFD",
+//       "circle-radius": 5,
+//       "circle-stroke-width": 1,
+//       "circle-stroke-color": "#fff",
+//     },
+//   });
+
+//   // inspect a cluster on click
+//   map.on("click", "clusters", (e) => {
+//     const features = map.queryRenderedFeatures(e.point, {
+//       layers: ["clusters"],
+//     });
+//     const clusterId = features[0].properties.cluster_id;
+//     map
+//       .getSource("restaurants")
+//       .getClusterExpansionZoom(clusterId, (err, zoom) => {
+//         if (err) return;
+
+//         map.easeTo({
+//           center: features[0].address.geometry.coordinates,
+//           zoom: zoom,
+//         });
+//       });
+//   });
+
+//   map.on("click", "unclustered-point", (e) => {
+//     const coordinates = e.features[0].address.geometry.coordinates.slice();
+//     const { popUpMarkup } = e.features[0].properties;
+//     while (Math.abs(e.lngLat.lng - coordinates[0]) > 180) {
+//       coordinates[0] += e.lngLat.lng > coordinates[0] ? 360 : -360;
+//     }
+
+//     new mapboxgl.Popup().setLngLat(coordinates).setHTML(popUpMarkup).addTo(map);
+//   });
+
+//   map.on("mouseenter", "clusters", () => {
+//     map.getCanvas().style.cursor = "pointer";
+//   });
+//   map.on("mouseleave", "clusters", () => {
+//     map.getCanvas().style.cursor = "";
+//   });
+// });
