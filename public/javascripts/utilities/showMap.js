dffff5c7cf95bdb7d432944a7e11f4f382e98acf
Hjalmar-K-sson <45142969+Hjalmar-K-sson@users.noreply.github.com>
2022-01-16 10:47:33 +0100

    Updated map data structure to be compatible with updated restaurant model

--- public/javascripts/utilities/showMap.js
+++ public/javascripts/utilities/showMap.js
@@ -2,13 +2,13 @@ mapboxgl.accessToken = mapboxToken;
 const map = new mapboxgl.Map({
   container: "showMap", // container ID
   style: "mapbox://styles/mapbox/streets-v11", // style URL
-  center: restaurant.address.geometry.coordinates, // starting position [lng, lat]
+  center: restaurant.geometry.coordinates, // starting position [lng, lat]
   zoom: 9, // starting zoom
 });
 map.addControl(new mapboxgl.NavigationControl(), "bottom-right");
 
 new mapboxgl.Marker()
-  .setLngLat(restaurant.address.geometry.coordinates)
+  .setLngLat(restaurant.geometry.coordinates)
   .setPopup(
     new mapboxgl.Popup({ offset: 25 }).setHTML(
       `<h3>${restaurant.name}</h3><p>${restaurant.address.street} ${restaurant.address.houseNumber}, ${restaurant.address.city}</p>`
