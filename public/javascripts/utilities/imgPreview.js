a92b4f4a9e7f1beb39e9debee786b79422c5d116
Hjalmar-K-sson <45142969+Hjalmar-K-sson@users.noreply.github.com>
2022-01-29 13:14:10 +0100

    Updated image preview script for new/edit form

--- public/javascripts/utilities/imgPreview.js
+++ public/javascripts/utilities/imgPreview.js
@@ -1,8 +1,26 @@
 const fileInput = document.getElementById("images");
 const fileCount = document.getElementById("fileCount");
+const addedImages = document.getElementById("addedImages");
 const getFilesNumber = () => {
-  fileCount.textContent = `${fileInput.files.length} files chosen`;
-  fileCount.style.background = "rgb(0, 209, 182)";
-  fileCount.style.color = "rgb(255, 255, 255)";
-  fileCount.style.border = "none";
+  addedImages.innerHTML = "";
+  if (fileInput.files.length > 1) {
+    fileCount.textContent = `${fileInput.files.length} files chosen`;
+    fileCount.style.background = "rgb(0, 209, 182)";
+    fileCount.style.color = "rgb(255, 255, 255)";
+    fileCount.style.border = "none";
+  } else {
+    fileCount.textContent = `${fileInput.files.length} file chosen`;
+    fileCount.style.background = "rgb(0, 209, 182)";
+    fileCount.style.color = "rgb(255, 255, 255)";
+    fileCount.style.border = "none";
+  }
+  for (i of fileInput.files) {
+    let reader = new FileReader();
+    let img = document.createElement("img");
+    reader.onload = () => {
+      img.setAttribute("src", reader.result);
+    };
+    addedImages.appendChild(img);
+    reader.readAsDataURL(i);
+  }
 };
